7c686cd11d5a596b540c7eab88440da95e85672c
jlbuild <juliabuildbot@gmail.com>
2020-02-09 00:56:32 +0000

    Xorg_libXcursor_jll build 1.2.0+1

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -3,7 +3,6 @@ export libXcursor
 
 using Xorg_libXfixes_jll
 using Xorg_libXrender_jll
-using Xorg_util_macros_jll
 ## Global variables
 PATH = ""
 LIBPATH = ""
@@ -27,14 +26,18 @@ const libXcursor = "libXcursor.so.1"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"Xorg_libXcursor")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    append!.(Ref(PATH_list), (Xorg_libXfixes_jll.PATH_list, Xorg_libXrender_jll.PATH_list, Xorg_util_macros_jll.PATH_list,))
-    append!.(Ref(LIBPATH_list), (Xorg_libXfixes_jll.LIBPATH_list, Xorg_libXrender_jll.LIBPATH_list, Xorg_util_macros_jll.LIBPATH_list,))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
+    # then append them to our own.
+    foreach(p -> append!(PATH_list, p), (Xorg_libXfixes_jll.PATH_list, Xorg_libXrender_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (Xorg_libXfixes_jll.LIBPATH_list, Xorg_libXrender_jll.LIBPATH_list,))
 
-    global libXcursor_path = abspath(joinpath(artifact"Xorg_libXcursor", libXcursor_splitpath...))
+    global libXcursor_path = normpath(joinpath(artifact_dir, libXcursor_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
